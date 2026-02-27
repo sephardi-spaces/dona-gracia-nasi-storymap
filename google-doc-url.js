664a4a5dbb9aca77dfd17b1a3b922a9f2db455c6
Sephardi Spaces <sepharadspaces@gmail.com>
2026-02-27 10:38:41 -0500

    Update Google Sheets URL with specific gid parameter

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1swCSjyeP4cUYGBoup_SxYAlh-yY2eprVN95aMAVTw60/edit';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1swCSjyeP4cUYGBoup_SxYAlh-yY2eprVN95aMAVTw60/edit?gid=474785908#gid=474785908';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
